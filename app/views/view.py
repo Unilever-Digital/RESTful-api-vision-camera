9f5037c2c82c69688e71fc8f52e6fbd77a61691e
Dat <dat.lemindast@gmail.com>
2024-04-11 09:02:33 +0700

    change

--- app/views/view.py
+++ app/views/view.py
@@ -83,41 +83,6 @@ def qltdata_carton_bi():
         json_data = tableMongoDBFetch_100data(collection)
         return jsonify(json_data)
 
-@blog.route("/qltdata/counter-bottles-bi",  methods=["POST", "GET"])
-def qltdata_counter_bottles_bi():
-    if request.method == "POST":
-        mongo_conn = connectToMongoDB(database="Vision_Mas140")
-        collection = mongo_conn["Table_ResultCounterBottles"]
-
-        # Fetch data from MongoDB and transform to JSON
-        json_data = tableMongoDBFetch(collection)
-        return jsonify({"quality-counter-bottles": json_data})
-    elif request.method =="GET":
-        mongo_conn = connectToMongoDB(database="Vision_Mas140")
-        collection = mongo_conn["Table_ResultCounterBottles"]
-
-        # Fetch data from MongoDB and transform to JSON
-        json_data = tableMongoDBFetch_100data(collection)
-        return jsonify({"quality-counter-bottles": json_data})
-
-
-@blog.route("/qltdata/cap-bi",  methods=["POST", "GET"])
-def qltdata_cap_bi():
-    if request.method == "POST":
-        mongo_conn = connectToMongoDB(database="Vision_Mas140")
-        collection = mongo_conn["Table_ResultCap"]
-
-        # Fetch data from MongoDB and transform to JSON
-        json_data = tableMongoDBFetch(collection)
-        return jsonify({"quality-cap": json_data})
-    elif request.method =="GET":
-        mongo_conn = connectToMongoDB(database="Vision_Mas140")
-        collection = mongo_conn["Table_ResultCap"]
-
-        # Fetch data from MongoDB and transform to JSON
-        json_data = tableMongoDBFetch_100data(collection)
-        return jsonify({"quality-cap": json_data})
-
 
 @blog.route("/user", methods=["POST", "GET"])
 def user():
